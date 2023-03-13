17d345285b073f7a9f95706addad0d21a2b38d78
ralphdegoma <personal.ralphdegoma@gmail.com>
2023-03-14 06:05:19 +0800

    fix scroll and active menu

--- dist/assets/AboutView-239af1b7.js
+++
@@ -1,1 +0,0 @@
-import{bs as e,at as t,p as s,q as o}from"./index-cb8baf36.js";const _={},a={class:"about"},c=o("h1",null,"This is an about page",-1),n=[c];function i(r,u){return t(),s("div",a,n)}const l=e(_,[["render",i]]);export{l as default};
