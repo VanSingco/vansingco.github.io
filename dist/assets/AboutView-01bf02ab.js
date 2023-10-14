b3f6989cb736a63678ee260144b945d63eaee813
Van Zachary Singco <vanske19@gmail.com>
2023-10-15 06:07:35 +0800

    Update layout

---
+++ dist/assets/AboutView-01bf02ab.js
@@ -0,0 +1,1 @@
+import{bv as e,at as t,p as s,q as o}from"./index-ae1fc743.js";const _={},a={class:"about"},c=o("h1",null,"This is an about page",-1),n=[c];function i(r,u){return t(),s("div",a,n)}const l=e(_,[["render",i]]);export{l as default};
